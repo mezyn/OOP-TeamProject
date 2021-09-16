8afa6d6edd88a7909f42729f2cc0a2dc16b91038
mezyn <84513662+mezyn@users.noreply.github.com>
2021-09-16 13:38:59 +0200

    commit the change

--- MainMenu.java
+++ MainMenu.java
@@ -1,29 +1,30 @@
 import java.util.Scanner;
 
-public class MainMenu {
+public class mainMenu {
     public static void main (String[] args){
 
-        Scanner OptionSc = new Scanner(System.in);
+        Scanner optionSc = new Scanner(System.in);
         System.out.println(
-                "Main Menu: Please chose among the options below \n" +
+                "Main Menu: Please choose among the options below. \n\n" +
                 "0. Close System.\n" +
-                "1. Open Item Options.\n" +
+                "1. Open Item options.\n" +
                 "2. Open Review options.\n" +
-                "3. Open Transaction History Options.\n" +
-                "Type any Option Number:");
+                "3. Open Transaction History options.\n\n" +
+                "Type any option number: ");
 
-        int counter = 0;
+        int inputOption = optionSc.nextInt();
+//making change
+        /*
+        if (inputOption == !0 && )
         do {
-            int option = OptionSc.nextInt();
-
-            switch (option) {
+            switch (inputOption) {
                 case 0 -> System.out.println("Closed"); // create method
-                case 1 -> Item.itemOption();
-                case 2 -> Review.ReviewMenu();
+                case 1 -> System.out.println("Item options");
+                case 2 -> System.out.println("Review options");
                 case 3 -> System.out.println("Transaction History");
                 default -> System.out.println("Please enter a valid option");
             }
         } while (counter >= 5) ;
-        OptionSc.close();
+        optionSc.close();*/
         }
     }
